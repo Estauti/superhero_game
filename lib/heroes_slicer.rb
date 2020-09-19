8038b0391b3846ef4a4d92ec674750c90534e640
Victor <estautib@gmail.com>
2020-09-18 22:33:36 -0300

    Add available hero ids to heroes slicer

--- lib/heroes_slicer.rb
+++ lib/heroes_slicer.rb
@@ -1,8 +1,15 @@
 class HeroesSlicer
-  attr_accessor :num_heroes
+  attr_reader :num_heroes, :available_hero_ids
   LAST_HERO_ID = 731.freeze
 
   def initialize(num_heroes)
     @num_heroes = num_heroes
+    @available_hero_ids = fill_available_hero_ids
+  end
+
+  private
+
+  def fill_available_hero_ids
+    @available_hero_ids = (1..LAST_HERO_ID).to_a
   end
 end
--- specs/heroes_slicer_spec.rb
+++ specs/heroes_slicer_spec.rb
@@ -22,4 +22,17 @@ RSpec.describe HeroesSlicer do
       expect(HeroesSlicer::LAST_HERO_ID).to be_kind_of(Integer)
     end
   end
+
+  context 'available_hero_ids' do
+    let(:available_hero_ids) { HeroesSlicer.new(5).available_hero_ids }
+
+    it 'is an array' do
+      expect(available_hero_ids).to be_instance_of(Array)
+    end
+
+    it 'is initialized with all hero ids' do
+      total_hero_ids = HeroesSlicer::LAST_HERO_ID
+      expect(available_hero_ids.length).to eq(total_hero_ids)
+    end
+  end
 end
