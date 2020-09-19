416cdc226df6b680d8ffd6c84ab4652f075d5007
Victor <estautib@gmail.com>
2020-09-18 21:22:17 -0300

    Get user input for how many cards each player will have

--- main.rb
+++ main.rb
@@ -1,1 +1,8 @@
+require "./heroes_per_player"
+
 p "Welcome to the Superheroe game!"
+
+p "How many cards for each player? (Maximum: 30)"
+heroes_per_player = HeroesPerPlayer.new(gets.chomp)
+
+puts heroes_per_player.number
