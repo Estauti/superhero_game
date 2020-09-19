b0878bf822bcf86889250c72f5b0f70469fa4382
Victor <estautib@gmail.com>
2020-09-18 23:24:14 -0300

    Add heroes slicer to main file

--- main.rb
+++ main.rb
@@ -1,8 +1,15 @@
 require "./lib/heroes_per_player"
+require "./lib/heroes_slicer"
 
 p "Welcome to the Superheroe game!"
 
 p "How many cards for each player? (Maximum: 30)"
 heroes_per_player = HeroesPerPlayer.new(gets.chomp)
 
-puts heroes_per_player.number
+heroes_slicer = HeroesSlicer.new(heroes_per_player.number)
+
+player = heroes_slicer.slice
+player_machine = heroes_slicer.slice
+
+p player
+p player_machine
